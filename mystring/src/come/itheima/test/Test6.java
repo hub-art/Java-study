bce6a64a97469d680b7c77af55b67dc234a6de92
幼儿园laoda <1378194676@qq.com>
2023-09-09 23:50:28 +0800

    java

---
+++ mystring/src/come/itheima/test/Test6.java
@@ -0,0 +1,25 @@
+package come.itheima.test;
+
+public class Test6 {
+    public static void main(String[] args) {
+        /*
+        给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。
+        返回字符串中最后一个单词的长度。
+        单词是指仅由字母组成、不包含任何空格字符的最大字符串。
+
+        示例1：输入：s = "Hello World"    输出：5
+        解释：最后一个单词"World",长度为5。
+
+        示例2：输入：s = ”    fly me  to  the moon"   输出：4
+        解释：最后一个单词是"moon",长度为4。
+
+        示例3：输入：s = "luffy is still joyboy"  输出：6
+        解释：最后一个单词是长度为6的"joyboy"。
+         */
+
+
+        //倒着遍历
+        //知道遇到空格为止
+        //那么遍历的次数就是单词的长度
+    }
+}
