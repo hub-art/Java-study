3f94500a9d7056f6929e296cb391fbffab4d005d
幼儿园laoda <1378194676@qq.com>
2023-09-22 21:43:04 +0800

    java

--- studentsystem/src/come/itheima/studentsystem/studentSystem1.java
+++ studentsystem/src/come/itheima/studentsystem/studentSystem1.java
@@ -13,7 +13,7 @@ public class studentSystem1 {
         Scanner sc = new Scanner(System.in);
         String number = sc.next();
         switch (number) {
-            case "1" -> register();
+            case "1" -> register(list);
             case "2" -> System.out.println("注册功能");
             case "3" -> System.out.println("忘记密码功能");
             default -> System.out.println("没有该选项");
@@ -22,10 +22,19 @@ public class studentSystem1 {
 
 
     //注册功能
-    public static void register() {
+    public static void register(ArrayList<User> list) {
         Scanner sc = new Scanner(System.in);
         String username = sc.next();
-        
+
+        if (username.length() >= 3 && username.length() <= 15) {
+            if (isUsernameUnique(list, username)) {
+                System.out.println("请输入密码");
+                String passworld = sc.next();
+
+            }
+        } else {
+            System.out.println("用户名长度不符合");
+        }
     }
 
     //登录功能
@@ -42,9 +51,25 @@ public class studentSystem1 {
     public static boolean isUsernameUnique(ArrayList<User> list, String username) {
         for (User u : list) {
             if (u.getUsername().equals(username)) {
-                return true;
+                return false;
             }
         }
-        return false;
+        return true;
+    }
+
+    //判断字符串组合
+    public static boolean isAlphaNumeric(String str) {
+        boolean hasLetter = false;
+        boolean hasDigit = false;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isLetter(c)) {
+                hasLetter = true;
+            } else if (Character.isDigit(c)) {
+                hasDigit = true;
+            }
+        }
+        return hasLetter && hasDigit;
     }
 }
